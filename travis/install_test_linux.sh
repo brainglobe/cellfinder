a60968ecb53e7d59b60ea55298df61015cbdfeb3
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-01-23 11:37:26 +0000

    retry twine upload

--- travis/install_test_linux.sh
+++ travis/install_test_linux.sh
@@ -1,15 +1,15 @@
-wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda info -a
-conda create -n test-environment python=$TRAVIS_PYTHON_VERSION
-source activate test-environment
-pip install -e .[dev]
-conda info -a
-black ./ -l 79 --check
-cellfinder -h
-cellfinder_download
-pytest --cov=cellfinder
-#ls
+#wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+#bash miniconda.sh -b -p $HOME/miniconda
+#export PATH="$HOME/miniconda/bin:$PATH"
+#hash -r
+#conda config --set always_yes yes --set changeps1 no
+#conda info -a
+#conda create -n test-environment python=$TRAVIS_PYTHON_VERSION
+#source activate test-environment
+#pip install -e .[dev]
+#conda info -a
+#black ./ -l 79 --check
+#cellfinder -h
+#cellfinder_download
+#pytest --cov=cellfinder
+ls
