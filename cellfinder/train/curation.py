2900ac8496b8fa7c52525c941d9e21f7e0780ece
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-02-12 14:40:56 +0000

    update curation

--- cellfinder/train/curation.py
+++ cellfinder/train/curation.py
@@ -232,9 +232,9 @@ def run_extraction(
     save_empty_cubes,
 ):
     planes_paths = {}
-    planes_paths[0] = get_sorted_file_paths(signal_paths, file_extension="tif")
+    planes_paths[0] = get_sorted_file_paths(signal_paths, file_extension=".tif")
     planes_paths[1] = get_sorted_file_paths(
-        background_paths, file_extension="tif"
+        background_paths, file_extension=".tif"
     )
 
     all_candidates = get_cells(str(output_filename))
