76eeb32d6945ffdcff7907b5821fcb05ca95878b
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-04-29 18:09:00 +0100

    update test script

--- travis/install_test_windows.sh
+++ travis/install_test_windows.sh
@@ -1,7 +1,7 @@
-pip install --upgrade pip
-pip install setuptools
-#pip install -e .[dev]
-#black .\ -l 79 --target-version py37 --check
-#cellfinder -h
-#cellfinder_download --atlas allen_2017_100um
-#pytest --cov=cellfinder
+#pip install --upgrade pip
+#pip install setuptools
+pip install -e .[dev]
+black .\ -l 79 --target-version py37 --check
+cellfinder -h
+cellfinder_download --atlas allen_2017_100um
+pytest --cov=cellfinder
