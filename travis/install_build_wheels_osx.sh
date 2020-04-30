219253d72f86f096a0643d2d73bd9137af0891b8
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-04-30 12:56:21 +0100

    edit osx deploy

--- travis/install_build_wheels_osx.sh
+++ travis/install_build_wheels_osx.sh
@@ -9,3 +9,6 @@ conda create -n test-environment python=$TRAVIS_PYTHON_VERSION
 source activate test-environment
 
 python setup.py bdist_wheel
+
+pip install twine
+twine upload dist/* --skip-existing
