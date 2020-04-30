1daf0eb0f1c0ffed83d1c468658c9abf9c3be301
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-04-30 10:23:25 +0100

    osx build

---
+++ travis/install_test_osx.sh
@@ -0,0 +1,14 @@
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda info -a
+conda create -n test-environment python=$TRAVIS_PYTHON_VERSION
+source activate test-environment
+pip install -e .[dev]
+conda info -a
+black ./ -l 79 --target-version py37 --check
+cellfinder -h
+cellfinder_download --atlas allen_2017_100um
+pytest --cov=cellfinder
