e17cb1fa920195ec490089081c4ef7bbce45310e
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-09-10 17:18:38 +0100

    update test scripts

--- travis/install_test_osx.sh
+++ travis/install_test_osx.sh
@@ -10,5 +10,5 @@ pip install -e .[dev]
 conda info -a
 black ./ -l 79 --target-version py37 --check
 cellfinder -h
-cellfinder_download --atlas allen_2017_100um
+cellfinder_download --atlas allen_mouse_100um
 pytest --cov=cellfinder
--- travis/test_windows.sh
+++ travis/test_windows.sh
@@ -4,5 +4,5 @@ source activate test-environment
 pip install -e .[dev]
 black .\ -l 79 --target-version py37 --check
 cellfinder -h
-cellfinder_download --atlas allen_2017_100um
+cellfinder_download --atlas allen_mouse_100um
 pytest --cov=cellfinder
