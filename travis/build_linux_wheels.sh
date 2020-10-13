14f7f44f106f77278ab47c3829001547e24deb04
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-10-13 13:44:21 +0100

    update docker script

--- travis/build_linux_wheels.sh
+++ travis/build_linux_wheels.sh
@@ -4,7 +4,7 @@ mkdir -p /io/wheelhouse/manylinux
 
 pip_paths=(
 	"/opt/python/cp37-cp37m/bin/pip"
-	"/opt/python/cp38-cp38m/bin/pip"
+	"/opt/python/cp38-cp38/bin/pip"
 	)
 
 for pip_path in "${pip_paths[@]}"; do
