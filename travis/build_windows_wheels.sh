e6e82c0f9f2a924cd78bbbdda5d93a1dfe6a6c03
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-11-25 12:17:27 +0000

    remove travis configs

--- travis/build_windows_wheels.sh
+++
@@ -1,4 +0,0 @@
-conda create -n test-environment python=$TRAVIS_PYTHON_VERSION
-source activate test-environment
-
-python setup.py bdist_wheel
--- travis/test_windows.sh
+++
@@ -1,8 +0,0 @@
-conda create -n test-environment python=$TRAVIS_PYTHON_VERSION
-source activate test-environment
-
-pip install -e .[dev]
-black .\ -l 79 --target-version py37 --check
-cellfinder -h
-brainglobe install -a allen_mouse_50um
-pytest --cov=cellfinder
