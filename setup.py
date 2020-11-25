b31e1e73c76b402fa737f1ec4177f4ba86d76e5d
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-11-25 11:38:28 +0000

    test deployment8

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ else:
 
 setup(
     name="cellfinder",
-    version="0.4.7rc4",
+    version="0.4.7rc5",
     description="Automated 3D cell detection and registration of "
     "whole-brain images",
     long_description=long_description,
