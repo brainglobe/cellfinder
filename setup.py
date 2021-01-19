03c6d18021805f4ccca00450a4e12458d1eff19d
Adam Tyson <adam.tyson@ucl.ac.uk>
2021-01-19 17:46:58 +0000

    update readme

--- setup.py
+++ setup.py
@@ -114,8 +114,8 @@ setup(
     ],
     url="https://cellfinder.info",
     project_urls={
-        "Source Code": "https://github.com/brainglobe/cellfinder",
-        "Bug Tracker": "https://github.com/brainglobe/cellfinder/issues",
+        "Source Code": "https://github.com/brainglobe/cellfinder-core",
+        "Bug Tracker": "https://github.com/brainglobe/cellfinder-core/issues",
         "Documentation": "https://docs.brainglobe.info/cellfinder",
     },
     author="Adam Tyson, Christian Niedworok, Charly Rousseau",
