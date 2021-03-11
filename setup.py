4a5a4910c02d444cd9a64a3981712c3fb68c3370
Adam Tyson <adam.tyson@ucl.ac.uk>
2021-03-11 09:19:54 +0000

    update deployment workflow

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ requirements = [
 
 setup(
     name="cellfinder",
-    version="6.2.001",
+    version="5.0.0",
     description="Automated 3D cell detection and registration of "
     "whole-brain images",
     long_description=long_description,
