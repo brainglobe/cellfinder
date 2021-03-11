0a777ec7a86eb3ac2d5749d9097cc0b047bbe728
Adam Tyson <adam.tyson@ucl.ac.uk>
2021-03-11 08:54:34 +0000

    Bump version: 0.5.0rc1 → 0.5.0rc2

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ requirements = [
 
 setup(
     name="cellfinder",
-    version="0.5.0rc1",
+    version="0.5.0rc2",
     description="Automated 3D cell detection and registration of "
     "whole-brain images",
     long_description=long_description,
