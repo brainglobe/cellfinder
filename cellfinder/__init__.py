dacff2a9509ca3d27f99f716167f131a694e10b1
Adam Tyson <adam.tyson@ucl.ac.uk>
2021-03-11 09:01:55 +0000

    Bump version: 0.5.dev → 0.6.dev

--- cellfinder/__init__.py
+++ cellfinder/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.5.dev"
+__version__ = "0.6.dev"
 __author__ = "Adam Tyson, Christian Niedworok, Charly Rousseau"
 __license__ = "GPL-3.0"
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ requirements = [
 
 setup(
     name="cellfinder",
-    version="0.5.dev",
+    version="0.6.dev",
     description="Automated 3D cell detection and registration of "
     "whole-brain images",
     long_description=long_description,
