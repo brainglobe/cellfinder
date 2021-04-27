cb2eca49cd2ef1a1e943f976a285a11d2d801929
Adam Tyson <code@adamltyson.com>
2021-04-27 15:51:10 +0100

    Bump version: 0.0.7 → 0.0.8-rc0

--- cellfinder_napari/__init__.py
+++ cellfinder_napari/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.7"
+__version__ = "0.0.8-rc0"
 __author__ = "Adam Tyson"
 __license__ = "GPL-3.0"
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
 
 setup(
     name="cellfinder-napari",
-    version="0.0.7",
+    version="0.0.8-rc0",
     author="Adam Tyson",
     author_email="code@adamltyson.com",
     license="GPL-3.0",
