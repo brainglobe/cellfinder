dbb6d395c3ca6dc6c3db8d8aa351cdd3e3b43600
Adam Tyson <code@adamltyson.com>
2021-06-10 18:06:18 +0100

    Bump version: 0.0.10 → 0.0.11-rc0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
 
 setup(
     name="cellfinder-napari",
-    version="0.0.10",
+    version="0.0.11-rc0",
     author="Adam Tyson",
     author_email="code@adamltyson.com",
     license="GPL-3.0",
