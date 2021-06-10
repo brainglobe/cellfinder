932960f6e45fcc4061b0806ddeb62ac6e44f492a
Adam Tyson <code@adamltyson.com>
2021-06-10 15:40:43 +0100

    pin to latest version of cellfinder-core

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 requirements = [
-    "cellfinder-core",
+    "cellfinder-core>=0.2.3",
     "numpy",
     "scikit-learn",
     "configparser",
