c6fc6c6d24d13761a33be900d2c77ca2cdfb2cde
alessandrofelder <a.felder@ucl.ac.uk>
2022-02-11 17:30:36 +0000

    consistently use lowercase variable names internally

--- cellfinder_napari/detect.py
+++ cellfinder_napari/detect.py
@@ -59,8 +59,8 @@ def detect():
             label="Voxel size (x)",
             step=0.1,
         ),
-        Soma_diameter=dict(
-            value=DEFAULT_PARAMETERS["Soma_diameter"], step=0.1
+        soma_diameter=dict(
+            value=DEFAULT_PARAMETERS["soma_diameter"], step=0.1
         ),
         ball_xy_size=dict(
             value=DEFAULT_PARAMETERS["ball_xy_size"], label="Ball filter (xy)"
@@ -68,27 +68,27 @@ def detect():
         ball_z_size=dict(
             value=DEFAULT_PARAMETERS["ball_z_size"], label="Ball filter (z)"
         ),
-        Ball_overlap=dict(value=DEFAULT_PARAMETERS["Ball_overlap"], step=0.1),
-        Filter_width=dict(value=DEFAULT_PARAMETERS["Filter_width"], step=0.1),
-        Threshold=dict(value=DEFAULT_PARAMETERS["Threshold"], step=0.1),
-        Cell_spread=dict(value=DEFAULT_PARAMETERS["Cell_spread"], step=0.1),
-        Max_cluster=dict(
-            value=DEFAULT_PARAMETERS["Max_cluster"], min=0, max=10000000
+        ball_overlap=dict(value=DEFAULT_PARAMETERS["ball_overlap"], step=0.1),
+        filter_width=dict(value=DEFAULT_PARAMETERS["filter_width"], step=0.1),
+        threshold=dict(value=DEFAULT_PARAMETERS["threshold"], step=0.1),
+        cell_spread=dict(value=DEFAULT_PARAMETERS["cell_spread"], step=0.1),
+        max_cluster=dict(
+            value=DEFAULT_PARAMETERS["max_cluster"], min=0, max=10000000
         ),
-        Trained_model=dict(value=DEFAULT_PARAMETERS["Trained_model"]),
-        Start_plane=dict(
-            value=DEFAULT_PARAMETERS["Start_plane"], min=0, max=100000
+        trained_model=dict(value=DEFAULT_PARAMETERS["trained_model"]),
+        start_plane=dict(
+            value=DEFAULT_PARAMETERS["start_plane"], min=0, max=100000
         ),
-        End_plane=dict(
-            value=DEFAULT_PARAMETERS["End_plane"], min=0, max=100000
+        end_plane=dict(
+            value=DEFAULT_PARAMETERS["end_plane"], min=0, max=100000
         ),
-        Number_of_free_cpus=dict(
-            value=DEFAULT_PARAMETERS["Number_of_free_cpus"]
+        number_of_free_cpus=dict(
+            value=DEFAULT_PARAMETERS["number_of_free_cpus"]
         ),
-        Analyse_local=dict(
-            value=DEFAULT_PARAMETERS["Analyse_local"], label="Analyse local"
+        analyse_local=dict(
+            value=DEFAULT_PARAMETERS["analyse_local"], label="Analyse local"
         ),
-        Debug=dict(value=DEFAULT_PARAMETERS["Debug"]),
+        debug=dict(value=DEFAULT_PARAMETERS["debug"]),
         # Classification_batch_size=dict(max=4096),
         call_button=True,
         persist=True,
@@ -99,37 +99,37 @@ def detect():
         detection_label,
         data_options,
         viewer: napari.Viewer,
-        Signal_image: napari.layers.Image,
-        Background_image: napari.layers.Image,
+        signal_image: napari.layers.Image,
+        background_image: napari.layers.Image,
         voxel_size_z: float,
         voxel_size_y: float,
         voxel_size_x: float,
         detection_options,
-        Soma_diameter: float,
+        soma_diameter: float,
         ball_xy_size: float,
         ball_z_size: float,
-        Ball_overlap: float,
-        Filter_width: float,
-        Threshold: int,
-        Cell_spread: float,
-        Max_cluster: int,
+        ball_overlap: float,
+        filter_width: float,
+        threshold: int,
+        cell_spread: float,
+        max_cluster: int,
         classification_options,
-        Trained_model: Path,
+        trained_model: Path,
         misc_options,
-        Start_plane: int,
-        End_plane: int,
-        Number_of_free_cpus: int,
-        Analyse_local: bool,
-        Debug: bool,
+        start_plane: int,
+        end_plane: int,
+        number_of_free_cpus: int,
+        analyse_local: bool,
+        debug: bool,
         reset_button,
     ) -> List[napari.types.LayerDataTuple]:
         """
 
         Parameters
         ----------
-        Signal_image : napari.layers.Image
+        signal_image : napari.layers.Image
              Image layer containing the labelled cells
-        Background_image : napari.layers.Image
+        background_image : napari.layers.Image
              Image layer without labelled cells
         voxel_size_z : float
             Size of your voxels in the axial dimension
@@ -137,47 +137,47 @@ def detect():
             Size of your voxels in the y direction (top to bottom)
         voxel_size_x : float
             Size of your voxels in the x direction (left to right)
-        Soma_diameter : float
+        soma_diameter : float
             The expected in-plane soma diameter (microns)
         ball_xy_size : float
             Elliptical morphological in-plane filter size (microns)
         ball_z_size : float
             Elliptical morphological axial filter size (microns)
-        Ball_overlap : float
+        ball_overlap : float
             Fraction of the morphological filter needed to be filled
             to retain a voxel
-        Filter_width : float
+        filter_width : float
             Laplacian of Gaussian filter width (as a fraction of soma diameter)
-        Threshold : int
+        threshold : int
             Cell intensity threshold (as a multiple of noise above the mean)
-        Cell_spread : float
+        cell_spread : float
             Cell spread factor (for splitting up cell clusters)
-        Max_cluster : int
+        max_cluster : int
             Largest putative cell cluster (in cubic um) where splitting
             should be attempted
-        Trained_model : Path
+        trained_model : Path
             Trained model file path (home directory (default) -> pretrained weights)
-        Start_plane : int
+        start_plane : int
             First plane to process (to process a subset of the data)
-        End_plane : int
+        end_plane : int
             Last plane to process (to process a subset of the data)
-        Number_of_free_cpus : int
+        number_of_free_cpus : int
             How many CPU cores to leave free
-        Analyse_local : bool
+        analyse_local : bool
             Only analyse planes around the current position
-        Debug : bool
+        debug : bool
             Increase logging
         reset_button :
             Reset parameters to default
         """
 
-        if End_plane == 0:
-            End_plane = len(Signal_image.data)
+        if end_plane == 0:
+            end_plane = len(signal_image.data)
 
-        if Trained_model == Path.home():
-            Trained_model = None
+        if trained_model == Path.home():
+            trained_model = None
 
-        if Analyse_local:
+        if analyse_local:
             current_plane = viewer.dims.current_step[0]
 
             # so a reasonable number of cells in the plane are detected
@@ -185,35 +185,35 @@ def detect():
                 ceil((CUBE_DEPTH * NETWORK_VOXEL_SIZES[0]) / voxel_size_z)
             )
 
-            Start_plane, End_plane = get_cube_depth_min_max(
+            start_plane, end_plane = get_cube_depth_min_max(
                 current_plane, planes_needed
             )
-            Start_plane = max(0, Start_plane)
-            End_plane = min(len(Signal_image.data), End_plane)
+            start_plane = max(0, start_plane)
+            end_plane = min(len(signal_image.data), end_plane)
 
         # initialise input
         voxel_sizes = (voxel_size_z, voxel_size_y, voxel_size_x)
-        dataInputs = DataInputs(Signal_image.data, \
-            Background_image.data,
+        dataInputs = DataInputs(signal_image.data, \
+            background_image.data,
             voxel_sizes)
 
         detectionInputs = DetectionInputs( \
-            Start_plane, 
-            End_plane,
-            Soma_diameter,
+            start_plane, 
+            end_plane,
+            soma_diameter,
             ball_xy_size,
             ball_z_size, 
-            Ball_overlap,
-            Filter_width,
-            Threshold,
-            Cell_spread,
-            Max_cluster)
+            ball_overlap,
+            filter_width,
+            threshold,
+            cell_spread,
+            max_cluster)
 
         worker = run(
             dataInputs,
             detectionInputs,
-            Trained_model,
-            Number_of_free_cpus,
+            trained_model,
+            number_of_free_cpus,
             # Classification_batch_size,
         )
         worker.returned.connect(lambda points : add_layers(points, viewer=viewer))
