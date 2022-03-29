0311b35254344944a4f0ea870e93fdef6c5cf4bd
David Stansby <dstansby@gmail.com>
2022-03-29 18:41:52 +0100

    Add simple smoke test for the main worker

--- cellfinder_napari/tests/test_detection.py
+++ cellfinder_napari/tests/test_detection.py
@@ -7,51 +7,37 @@ import pytest
 from PIL import Image
 
 from cellfinder_napari.detect import detect
+from cellfinder_napari.detect.input_containers import (
+    ClassificationInputs,
+    DataInputs,
+    DetectionInputs,
+    MiscInputs,
+)
+from cellfinder_napari.detect.thread_worker import Worker
+from cellfinder_napari.sample_data import load_sample
 
-test_data_dir = pathlib.Path(__file__) / ".." / ".." / ".." / "test_data"
 
-
-def load_stack(directory: pathlib.Path) -> np.ndarray:
+def test_add_detect_widget(make_napari_viewer):
     """
-    Load a stack of .tif image files in the given directory.
+    Smoke test to check that adding detection widget works
     """
-    tif_glob = str((directory / "*.tif").resolve())
-    ims = []
-    for f in sorted(glob.glob(tif_glob)):
-        ims.append(np.array(Image.open(f)))
-    return np.stack(ims, axis=0)
-
-
-# @pytest.fixture
-# def signal_image():
-#    return load_stack(test_data_dir / "crop_planes" / "ch0")
-
-
-# @pytest.fixture
-# def background_image():
-#    return load_stack(test_data_dir / "crop_planes" / "ch1")
-
-
-def test_detect_function(
-    make_napari_viewer,
-):  # , signal_image, background_image):
-    # Smoke test that running cell detection from the widget works
-
-    # Create viewer and add images
     viewer = make_napari_viewer()
-    # signal_layer = viewer.add_image(signal_image, name='signal')
-    # background_layer = viewer.add_image(background_image, name='background')
-
-    # Create widget
     widget = detect()
     viewer.window.add_dock_widget(widget)
 
-    # Select images, and run detection
-    widget.reset_choices()
-    # widget.signal_image.value = signal_layer
-    # widget.background_image.value = background_layer
 
-    # TODO: work out how to run detection here whilst blocking execution.
-    # Currently calling widget() will start detection, but pytest will
-    # pass immediately because new processes are spawned to do the detection.
-    # Perhaps use https://pytest-qt.readthedocs.io/en/latest/signals.html ?
+def test_detect_worker():
+    """
+    Smoke test to check that the detection worker runs
+    """
+    data = load_sample()
+    signal = data[0][0]
+    background = data[1][0]
+
+    worker = Worker(
+        DataInputs(signal_array=signal, background_array=background),
+        DetectionInputs(),
+        ClassificationInputs(trained_model=None),
+        MiscInputs(end_plane=20),
+    )
+    worker.work()
