f75584bdb1fbdbaff621a1f815a86bc9b08e088a
David Stansby <dstansby@gmail.com>
2022-04-21 16:25:22 +0100

    Bump version: 0.3.0-rc2 → 0.3.0-rc3

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ extensions = [
 
 setup(
     name="cellfinder-core",
-    version="0.3.0-rc2",
+    version="0.3.0-rc3",
     description="Automated 3D cell detection in large microscopy images",
     long_description=long_description,
     long_description_content_type="text/markdown",
