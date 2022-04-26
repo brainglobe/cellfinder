cb62407263bfa25503219c9cb9d4ceac591f4511
David Stansby <dstansby@gmail.com>
2022-04-26 15:01:13 +0100

    Test adding new training layers for cell curation (#105)
    * Test adding new training layers for cell curation
    
    * Update cellfinder_napari/tests/test_curation.py
    
    Co-authored-by: Patrick Roddy <patrickjamesroddy@gmail.com>
    
    Co-authored-by: Patrick Roddy <patrickjamesroddy@gmail.com>

--- cellfinder_napari/tests/test_curation.py
+++ cellfinder_napari/tests/test_curation.py
@@ -1,11 +1,31 @@
+import pytest
+from napari.layers import Points
+
 from cellfinder_napari import sample_data
 from cellfinder_napari.curation import CurationWidget
 
 
-def test_detect_function(make_napari_viewer):
+@pytest.fixture
+def curation_widget(make_napari_viewer):
     """
-    Smoke test that adding the curation widget to napari works
+    Create a viewer, add the curation widget, and return the widget.
+    The viewer can be accessed using ``widget.viewer``.
     """
     viewer = make_napari_viewer()
     widget = CurationWidget(viewer)
     viewer.window.add_dock_widget(widget)
+    return widget
+
+
+def test_add_new_training_layers(curation_widget):
+    viewer = curation_widget.viewer
+    layers = viewer.layers
+    # Check that layers list starts off empty
+    assert len(layers) == 0
+    curation_widget.add_training_data()
+    assert len(layers) == 2
+
+    assert all(isinstance(layer, Points) for layer in layers)
+
+    assert layers[0].name == "Training data (cells)"
+    assert layers[1].name == "Training data (non cells)"
