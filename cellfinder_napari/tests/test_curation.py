6cabda9ff0e307241b4c238c4cd979ba60bc28e5
David Stansby <dstansby@gmail.com>
2022-04-28 12:20:10 +0100

    Test adding cells and non-cells during curation. (#106)
    * Test that marking cells and non-cells works in curation
    
    * Improve test variable names

--- cellfinder_napari/tests/test_curation.py
+++ cellfinder_napari/tests/test_curation.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 from napari.layers import Points
 
@@ -29,3 +30,35 @@ def test_add_new_training_layers(curation_widget):
 
     assert layers[0].name == "Training data (cells)"
     assert layers[1].name == "Training data (non cells)"
+
+
+def test_cell_marking(curation_widget):
+    """
+    Check that marking cells and non-cells works as expected.
+    """
+    widget = curation_widget
+    widget.add_training_data()
+    viewer = widget.viewer
+
+    cell_layer = widget.training_data_cell_layer
+    non_cell_layer = widget.training_data_non_cell_layer
+
+    # Check that no cells have been marked yet
+    assert all(
+        layer.data.shape == (0, 3) for layer in [cell_layer, non_cell_layer]
+    )
+
+    # Add a points layer to select points from
+    points = Points(np.array([[0, 1, 2], [3, 4, 5]]), name="selection_points")
+    # Adding the layer automatically selects it in the layer list
+    viewer.add_layer(points)
+
+    points.selected_data = [0]
+    curation_widget.mark_as_cell()
+    assert np.array_equal(cell_layer.data, np.array([[0, 1, 2]]))
+    assert non_cell_layer.data.shape[0] == 0
+
+    points.selected_data = [1]
+    curation_widget.mark_as_non_cell()
+    assert np.array_equal(cell_layer.data, np.array([[0, 1, 2]]))
+    assert np.array_equal(non_cell_layer.data, np.array([[3, 4, 5]]))
