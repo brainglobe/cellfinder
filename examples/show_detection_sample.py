c8f926f50706923c567351600ee5a3697e866a2c
David Stansby <dstansby@gmail.com>
2022-05-05 14:11:08 +0100

    Update examples/show_detection_sample.py
    Co-authored-by: Alessandro Felder <alessandrofelder@users.noreply.github.com>

--- examples/show_detection_sample.py
+++ examples/show_detection_sample.py
@@ -14,7 +14,7 @@ from cellfinder_napari.sample_data import load_sample
 viewer = napari.Viewer()
 # Open plugin
 viewer.window.add_plugin_dock_widget(
-    plugin_name="cellfinder-napari", widget_name="Train network"
+    plugin_name="cellfinder-napari", widget_name="Cell detection"
 )
 # Add sample data layers
 for layer in load_sample():
