b1a5346b2efd1f3182f4016905eef04d00bc2510
alessandrofelder <a.felder@ucl.ac.uk>
2022-06-27 16:42:12 +0100

    add tests for reset and run buttons

--- cellfinder_napari/tests/test_training.py
+++ cellfinder_napari/tests/test_training.py
@@ -1,39 +1,84 @@
 from pathlib import Path
+from unittest.mock import patch
 
 import pytest
 
-from cellfinder_napari.train import train
+from cellfinder_napari.train.train import training_widget
+from cellfinder_napari.train.train_containers import (
+    MiscTrainingInputs,
+    OptionalNetworkInputs,
+    OptionalTrainingInputs,
+    TrainingDataInputs,
+)
 
 
 @pytest.fixture
-def training_widget(make_napari_viewer):
+def get_training_widget(make_napari_viewer):
     viewer = make_napari_viewer()
-    widget = train()
+    widget = training_widget()
     viewer.window.add_dock_widget(widget)
     return widget
 
 
-def test_add_training_widget(training_widget):
+def test_add_training_widget(get_training_widget):
     """
     Smoke test to check that adding training widget works
     """
-    widget = training_widget
+    widget = get_training_widget
     assert widget is not None
 
 
-def test_reset_to_defaults(training_widget):
+def test_reset_to_defaults(get_training_widget):
     # change a few widgets to non-default values
-    training_widget.yaml_files.value = ["file_1.yml", "file_2.yml"]
-    training_widget.continue_training.value = True
-    training_widget.epochs.value = 50
-    training_widget.test_fraction.value = 0.20
+    get_training_widget.yaml_files.value = ["file_1.yml", "file_2.yml"]
+    get_training_widget.continue_training.value = True
+    get_training_widget.epochs.value = 50
+    get_training_widget.test_fraction.value = 0.20
 
     # click reset button
-    training_widget.reset_button.clicked()
+    get_training_widget.reset_button.clicked()
 
     # check values have been reset
-    assert len(training_widget.yaml_files.value) == 1
-    assert training_widget.yaml_files.value[0] == Path.home()
-    assert not training_widget.continue_training.value
-    assert training_widget.epochs.value == 100
-    assert training_widget.test_fraction.value == 0.10
+    assert len(get_training_widget.yaml_files.value) == 1
+    assert get_training_widget.yaml_files.value[0] == Path.home()
+    assert not get_training_widget.continue_training.value
+    assert get_training_widget.epochs.value == 100
+    assert get_training_widget.test_fraction.value == 0.10
+
+
+def test_run_with_no_yaml_files(get_training_widget):
+    with patch("cellfinder_napari.train.train.show_info") as show_info:
+        get_training_widget.call_button.clicked()
+        show_info.assert_called_once_with(
+            "Please select a YAML file for training"
+        )
+
+
+def test_run_with_virtual_yaml_files(get_training_widget):
+    with patch("cellfinder_napari.train.train.run_training") as run_training:
+        # make default input valid - need yml files (they don't technically have to exist)
+        virtual_yaml_files = (
+            Path.home() / "file_1.yml",
+            Path.home() / "file_2.yml",
+        )
+        get_training_widget.yaml_files.value = virtual_yaml_files
+        get_training_widget.call_button.clicked()
+
+        # create expected arguments for run
+        expected_training_args = TrainingDataInputs()
+        expected_network_args = OptionalNetworkInputs()
+        expected_optional_training_args = OptionalTrainingInputs()
+        expected_misc_args = MiscTrainingInputs()
+
+        # we expect the widget to make some changes to the defaults
+        # displayed before calling the training backed
+        expected_training_args.yaml_files = virtual_yaml_files
+        expected_network_args.trained_model = None
+        expected_network_args.model_weights = None
+
+        run_training.assert_called_once_with(
+            expected_training_args,
+            expected_network_args,
+            expected_optional_training_args,
+            expected_misc_args,
+        )
