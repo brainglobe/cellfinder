3026bf1724c25998dc020c077f8e6518d3c19a8c
alessandrofelder <a.felder@ucl.ac.uk>
2022-06-28 17:19:46 +0100

    use set to check core_args are pass as expected.

--- cellfinder_napari/tests/test_train_containers.py
+++ cellfinder_napari/tests/test_train_containers.py
@@ -28,7 +28,8 @@ def test_core_args_passed(input_container):
     to the training backend actually is also expected by the backend
     """
     backend_signature = signature(run)
-    expected_kwargs_list = list(backend_signature.parameters.keys())
-    actual_kwargs_list = list(input_container.as_core_arguments().keys())
-    for key in actual_kwargs_list:
-        assert key in expected_kwargs_list
+    expected_kwargs_set = set(backend_signature.parameters.keys())
+    actual_kwargs_set = set(input_container.as_core_arguments().keys())
+    assert (
+        actual_kwargs_set <= expected_kwargs_set
+    )  # check all actual keywords are in expected (but not the other way around.)
