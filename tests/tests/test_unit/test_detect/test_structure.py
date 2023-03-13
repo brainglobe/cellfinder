e379aed8e25c68d0794d93de7ef8d35e54b9ed8e
David Stansby <dstansby@gmail.com>
2023-03-13 13:58:29 +0000

    Add some more connected test cases

--- tests/tests/test_unit/test_detect/test_structure.py
+++ tests/tests/test_unit/test_detect/test_structure.py
@@ -15,9 +15,36 @@ depth = 2
 # - A dict of expected structure coordinates
 test_data = [
     (
-        # Two pixels connected in a single structure
+        # No pixels
+        [],
+        {},
+    ),
+    (
+        # Two pixels connected in a single structure along x
         [(0, 0, 0), (0, 1, 0)],
         {1: [{"x": 0, "y": 0, "z": 0}, {"x": 1, "y": 0, "z": 0}]},
+    ),
+    (
+        # Two pixels connected in a single structure along y
+        [(0, 0, 0), (0, 0, 1)],
+        {1: [{"x": 0, "y": 0, "z": 0}, {"x": 0, "y": 1, "z": 0}]},
+    ),
+    (
+        # Two pixels connected in a single structure along z
+        [(0, 0, 0), (1, 0, 0)],
+        {1: [{"x": 0, "y": 0, "z": 0}, {"x": 0, "y": 0, "z": 1}]},
+    ),
+    (
+        # Four pixels all connected and spread across x-y-z
+        [(0, 0, 0), (1, 0, 0), (1, 1, 0), (1, 0, 1)],
+        {
+            1: [
+                {"x": 0, "y": 0, "z": 0},
+                {"x": 0, "y": 0, "z": 1},
+                {"x": 1, "y": 0, "z": 1},
+                {"x": 0, "y": 1, "z": 1},
+            ]
+        },
     ),
     (
         # Two disconnected single-pixel structures
