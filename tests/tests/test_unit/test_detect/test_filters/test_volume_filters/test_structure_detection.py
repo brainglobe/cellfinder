ff1530b461a645b3e7ccd6a0a96c995f28f7cf11
David Stansby <dstansby@gmail.com>
2023-03-14 11:21:22 +0000

    Add two more test cases

--- tests/tests/test_unit/test_detect/test_filters/test_volume_filters/test_structure_detection.py
+++ tests/tests/test_unit/test_detect/test_filters/test_volume_filters/test_structure_detection.py
@@ -92,6 +92,19 @@ test_data = [
         },
     ),
     (
+        # three initially disconnected pixels that then get merged
+        # by a fourth pixel
+        [(1, 1, 0), (0, 1, 1), (1, 0, 1), (1, 1, 1)],
+        {
+            1: [
+                {"x": 1, "y": 1, "z": 0},
+                {"x": 0, "y": 1, "z": 1},
+                {"x": 1, "y": 0, "z": 1},
+                {"x": 1, "y": 1, "z": 1},
+            ]
+        },
+    ),
+    (
         # Three pixels in x-y plane that require structure merging
         [(1, 0, 0), (0, 1, 0), (1, 1, 0)],
         {
@@ -106,6 +119,11 @@ test_data = [
         # Two disconnected single-pixel structures
         [(0, 0, 0), (0, 2, 0)],
         {1: [{"x": 0, "y": 0, "z": 0}], 2: [{"x": 2, "y": 0, "z": 0}]},
+    ),
+    (
+        # Two disconnected single-pixel structures along a diagonal
+        [(0, 0, 0), (1, 1, 1)],
+        {1: [{"x": 0, "y": 0, "z": 0}], 2: [{"x": 1, "y": 1, "z": 1}]},
     ),
 ]
 
