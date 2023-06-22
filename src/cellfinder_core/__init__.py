b69cdef068d0b1c3801cdf7c4585a8f2bbe9f5ef
Will Graham <32364977+willGraham01@users.noreply.github.com>
2023-06-22 10:03:50 +0100

    Fix dynamic versioning (#177)
    * Fix dynamic versioning
    
    * pre-commit run
    
    * Apply suggestions from code review
    
    Co-authored-by: David Stansby <dstansby@gmail.com>
    
    * Pre-commit run
    
    ---------
    
    Co-authored-by: David Stansby <dstansby@gmail.com>

--- src/cellfinder_core/__init__.py
+++ src/cellfinder_core/__init__.py
@@ -1,4 +1,10 @@
-__version__ = "0.3.1"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("cellfinder-core")
+except PackageNotFoundError as e:
+    raise PackageNotFoundError("cellfinder-core package not installed") from e
+
 __author__ = "Adam Tyson, Christian Niedworok, Charly Rousseau"
 __license__ = "BSD-3-Clause"
 
