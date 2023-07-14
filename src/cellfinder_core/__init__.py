224e0b33477191a643f8e82719d516a8452467a9
Alessandro Felder <alessandrofelder@users.noreply.github.com>
2023-07-14 11:41:13 +0100

    possible fix for mac error (#200)

--- src/cellfinder_core/__init__.py
+++ src/cellfinder_core/__init__.py
@@ -10,12 +10,15 @@ except PackageNotFoundError as e:
 try:
     TF_VERSION = version("tensorflow")
 except PackageNotFoundError as e:
-    raise PackageNotFoundError(
-        f"cellfinder tools cannot be invoked without tensorflow. "
-        f"Please install tensorflow into your environment to use cellfinder tools. "
-        f"For more information, please see "
-        f"https://github.com/brainglobe/brainglobe-meta#readme."
-    ) from e
+    try:
+        TF_VERSION = version("tensorflow-macos")
+    except PackageNotFoundError as e:
+        raise PackageNotFoundError(
+            f"cellfinder tools cannot be invoked without tensorflow. "
+            f"Please install tensorflow into your environment to use cellfinder tools. "
+            f"For more information, please see "
+            f"https://github.com/brainglobe/brainglobe-meta#readme."
+        ) from e
 
 __author__ = "Adam Tyson, Christian Niedworok, Charly Rousseau"
 __license__ = "BSD-3-Clause"
