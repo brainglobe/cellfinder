9fe4ef8e9b91c4820b02d7b0f8a680047463d0ef
alessandrofelder <alessandrofelder@users.noreply.github.com>
2023-11-13 16:50:21 +0000

    distribute test points across region/hemisphere, save regression data
    regression data from pandas 1.5.3

--- tests/tests/test_unit/test_analyse.py
+++ tests/tests/test_unit/test_analyse.py
@@ -1,3 +1,4 @@
+import filecmp
 from pathlib import Path
 
 import pytest
@@ -17,20 +18,23 @@ def points():
     p2 = Point(
         raw_coordinate=[2.0, 2.0, 2.0],
         atlas_coordinate=[2.0, 2.0, 2.0],
-        structure="Paraventricular hypothalamic nucleus, descending division",
-        structure_id=56,
-        hemisphere="right",
+        structure="Anterodorsal nucleus",
+        structure_id=57,
+        hemisphere="left",
     )
     return [p1, p2]
 
 
 @pytest.fixture
 def structures_with_points():
-    return ["Paraventricular hypothalamic nucleus, descending division"]
+    return [
+        "Paraventricular hypothalamic nucleus, descending division",
+        "Anterodorsal nucleus",
+    ]
 
 
 def test_get_region_totals(points, structures_with_points):
-    """Smoke test for get_region_totals"""
+    """Regression test for get_region_totals"""
     volumes_path = Path("./tests/data/analyse/volumes.csv")
     output_path = Path("./tests/data/analyse/region_totals.csv")
     output_path.unlink(missing_ok=True)
@@ -38,3 +42,7 @@ def test_get_region_totals(points, structures_with_points):
         points, structures_with_points, volumes_path, output_path
     )
     assert output_path.exists()
+    expected_output = Path(
+        "./tests/data/analyse/region_totals_regression_pandas1_5_3.csv"
+    )
+    assert filecmp.cmp(output_path, expected_output)
