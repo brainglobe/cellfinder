e6a552114885d554cdc9a8ccef23ee12c78cae0a
willGraham01 <1willgraham@gmail.com>
2023-11-27 14:29:26 +0000

    Clean out cellfinder (old) to make way for merge

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM tensorflow/tensorflow:latest-gpu
-LABEL maintainer="code@adamltyson.com"
-RUN pip install cellfinder
-CMD ["bash"]
--- cellfinder/export/export.py
+++
@@ -1,9 +0,0 @@
-from cellfinder.export.abc4d import export_points as abc4d_export
-from cellfinder.export.brainrender import export_points as brainrender_export
-
-
-def export_points(
-    point_info, points, resolution, brainrender_points_path, abc4d_points_path
-):
-    brainrender_export(points, resolution, brainrender_points_path)
-    abc4d_export(point_info, resolution, abc4d_points_path)
