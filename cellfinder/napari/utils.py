1e8f98fd5ea440d70c2784d0982a171612905914
Adam Tyson <code@adamltyson.com>
2024-01-16 16:18:03 +0000

    Move qtpy functionality to brainglobe-utils

--- cellfinder/napari/utils.py
+++ cellfinder/napari/utils.py
@@ -1,18 +1,10 @@
-from typing import Callable, List, Optional, Tuple
+from typing import List, Tuple
 
 import napari
 import numpy as np
 import pandas as pd
 from brainglobe_utils.cells.cells import Cell
 from pkg_resources import resource_filename
-from qtpy.QtWidgets import (
-    QComboBox,
-    QLabel,
-    QLayout,
-    QMessageBox,
-    QPushButton,
-    QWidget,
-)
 
 brainglobe_logo = resource_filename(
     "cellfinder", "napari/images/brainglobe.png"
@@ -98,83 +90,3 @@ def cells_to_array(cells: List[Cell]) -> Tuple[np.ndarray, np.ndarray]:
     points = cells_df_as_np(df[df["type"] == Cell.CELL])
     rejected = cells_df_as_np(df[df["type"] == Cell.UNKNOWN])
     return points, rejected
-
-
-def add_combobox(
-    layout: QLayout,
-    label: str,
-    items: List[str],
-    row: int,
-    column: int = 0,
-    label_stack: bool = False,
-    callback=None,
-    width: int = 150,
-) -> Tuple[QComboBox, Optional[QLabel]]:
-    """
-    Add a selection box to *layout*.
-    """
-    if label_stack:
-        combobox_row = row + 1
-        combobox_column = column
-    else:
-        combobox_row = row
-        combobox_column = column + 1
-    combobox = QComboBox()
-    combobox.addItems(items)
-    if callback:
-        combobox.currentIndexChanged.connect(callback)
-    combobox.setMaximumWidth = width
-
-    if label is not None:
-        combobox_label = QLabel(label)
-        combobox_label.setMaximumWidth = width
-        layout.addWidget(combobox_label, row, column)
-    else:
-        combobox_label = None
-
-    layout.addWidget(combobox, combobox_row, combobox_column)
-    return combobox, combobox_label
-
-
-def add_button(
-    label: str,
-    layout: QLayout,
-    connected_function: Callable,
-    row: int,
-    column: int = 0,
-    visibility: bool = True,
-    minimum_width: int = 0,
-    alignment: str = "center",
-) -> QPushButton:
-    """
-    Add a button to *layout*.
-    """
-    button = QPushButton(label)
-    if alignment == "center":
-        pass
-    elif alignment == "left":
-        button.setStyleSheet("QPushButton { text-align: left; }")
-    elif alignment == "right":
-        button.setStyleSheet("QPushButton { text-align: right; }")
-
-    button.setVisible(visibility)
-    button.setMinimumWidth(minimum_width)
-    layout.addWidget(button, row, column)
-    button.clicked.connect(connected_function)
-    return button
-
-
-def display_question(widget: QWidget, title: str, message: str) -> bool:
-    """
-    Display a warning in a pop up that informs about overwriting files.
-    """
-    message_reply = QMessageBox.question(
-        widget,
-        title,
-        message,
-        QMessageBox.Yes | QMessageBox.Cancel,
-    )
-    if message_reply == QMessageBox.Yes:
-        return True
-    else:
-        return False
