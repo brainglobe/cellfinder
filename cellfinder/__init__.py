81e0bff42d4ff6bdedb5688f06feb12aeba83e2e
sfmig <33267254+sfmig@users.noreply.github.com>
2024-02-06 15:40:53 +0000

    check if Keras present

--- cellfinder/__init__.py
+++ cellfinder/__init__.py
@@ -5,20 +5,17 @@ try:
 except PackageNotFoundError as e:
     raise PackageNotFoundError("cellfinder package not installed") from e
 
-# If tensorflow is not present, tools cannot be used.
+# If Keras is not present with a backend, tools cannot be used.
 # Throw an error in this case to prevent invocation of functions.
 try:
-    TF_VERSION = version("tensorflow")
+    KERAS_VERSION = version("keras")  # replace by keras?
 except PackageNotFoundError as e:
-    try:
-        TF_VERSION = version("tensorflow-macos")
-    except PackageNotFoundError as e:
-        raise PackageNotFoundError(
-            f"cellfinder tools cannot be invoked without tensorflow. "
-            f"Please install tensorflow into your environment to use cellfinder tools. "
-            f"For more information, please see "
-            f"https://github.com/brainglobe/brainglobe-meta#readme."
-        ) from e
+    raise PackageNotFoundError(
+        f"cellfinder tools cannot be invoked without Keras. "
+        f"Please install tensorflow into your environment to use cellfinder tools. "
+        f"For more information, please see "
+        f"https://github.com/brainglobe/brainglobe-meta#readme."
+    ) from e
 
 __author__ = "Adam Tyson, Christian Niedworok, Charly Rousseau"
 __license__ = "BSD-3-Clause"
