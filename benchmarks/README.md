68cee276757c79c84a3e980030b7a0155789cf02
sfmig <33267254+sfmig@users.noreply.github.com>
2024-05-10 10:50:39 +0200

    Follow numpy structure for benchmarks

--- benchmarks/README.md
+++ benchmarks/README.md
@@ -1,12 +1,190 @@
-# Benchmarks
-`detect_and_classify.py` contains a simple script that runs
-detection and classification with the small test dataset.
+# README
 
-## Memory
-[memory_profiler](https://github.com/pythonprofilers/memory_profiler)
-can be used to profile memory useage. Install, and then run
-`mprof run --include-children --multiprocess detect_and_classify.py`. It is **very**
-important to use these two flags to capture memory usage by the additional
-processes that cellfinder.core uses.
+## Overview
+We use `asv` to benchmark some representative brainglobe workflows. To [install asv](https://asv.readthedocs.io/en/stable/installing.html):
+```
+pip install asv
+```
+Note that to run the benchmarks you only need to have `asv` in your virtual environment. You do not need to install a development version of `brainglobe-workflows` (since `asv` will create a separate Python virtual environment to run the benchmarks on it). However, for convenience we include `asv` as part of the `[dev]` dependencies, so an environment with `brainglobe-workflows[dev]` can be used to run the benchmarks.
 
-To show the results of the latest profile run, run `mprof plot`.
+
+The `asv` workflow is roughly as follows:
+1. `asv` creates a virtual environment (as defined in the `asv.conf.json` file).
+1. It installs the version of the `brainglobe-workflows` software package corresponding to the tip of the locally checked-out branch.
+1. It runs the benchmarks defined (locally) under `brainglobe-workflows/benchmarks/benchmarks` and saves the results to `brainglobe-workflows/benchmarks/results` as json files.
+1. With `asv publish`, the output json files are 'published' into an html directory (`brainglobe-workflows/benchmarks/html`)
+1. With `asv preview` the html directory can be visualised in a static site.
+
+
+There are three main ways in which these benchmarks can be useful to developers:
+1. Developers can run the available benchmarks locally on a small test dataset. See [here](#running-benchmarks-locally-on-default-small-dataset).
+1. Developers can run these benchmarks on data they have stored locally. See [here](#running-benchmarks-locally-on-custom-data).
+1. We also plan to run the benchmarks on an internal runner using a larger dataset, of the scale we expect users to be handling. The result of these benchmarks will be made publicly available.
+
+## Running benchmarks locally on default small dataset
+
+ To do so:
+    - Git clone repo locally
+    - Install the developer version of the package:
+        ```
+        pip install .[dev]
+        ```
+        This is mostly for convenience: the `[dev]` specification includes `asv` as a dependency, but to run the benchmarks it would be sufficient to use an environment with `asv` only. This is because `asv` creates its own virtual environment for the benchmarks, building and installing the relevant version of the `brainglobe-workflows` package in it. By default, the version at the tip of the currently checked out branch is installed.
+    - Run the benchmarks, From the directory where asv config is at:
+        ```
+        asv run
+        ```
+       This will run the locally defined benchmarks with the default parameters defined at `brainglobe_workflows/configs/cellfinder.json`, on a small dataset downloaded from [GIN](https://gin.g-node.org/G-Node/info/wiki). See the [asv docs](https://asv.readthedocs.io/en/v0.6.1/using.html#running-benchmarks) for further guidance on how to run benchmarks.
+
+
+## Running benchmarks locally on custom data
+For data available locally.
+
+Run commands From the directory where asv config is at.
+
+To do so:
+    - Git clone repo locally
+    - Define a config file for the workflow to benchmark. You can use the default one at `brainglobe_workflows/configs/cellfinder.json` for reference.
+    - Ensure your config file includes an `input_data_dir` field pointing to the data of interest.
+    - Edit the names of the signal and background directories if required. By default, they are assumed to be in `signal` and `background` subdirectories under `input_data_dir`. However, these defaults can be overwritten with the `signal_subdir` and `background_subdir` fields.
+    - Run the benchmarks, passing the path to your config file as an environment variable `CELLFINDER_CONFIG_PATH`. In Unix systems:
+        ```
+        CELLFINDER_CONFIG_PATH=/path/to/your/config/file asv run
+        ```
+
+## Quick run
+
+For a quick check, you can run one iteration per benchmark with
+```
+asv run -q
+```
+You can add -v --show-stderr for a more verbose output. --dry-run for not saving to disk.
+
+`asv run -q -v --show-stderr`
+
+In development, the following flags to `asv run` are often useful:
+- `--bench`: to specify a subset of benchmarks (e.g., `tools.prep.PrepTF`). Regexp can be used.
+- `--dry-run`: will not write results to disk
+- `--quick`: will only run one repetition, and no results to disk
+- `--show-stderr`: will print out stderr
+- `--verbose`: provides further info on intermediate steps
+- `--python=same`: runs the benchmarks in the same environment that `asv` was launched from
+
+E.g.:
+```
+asv run --bench bench tools.prep.PrepTF --dry-run --show-stderr --quick
+```
+
+
+
+## Useful commands for running benchmarks
+To run benchmarks on a specific commit:
+```
+$ asv run 88fbbc33^!
+```
+
+To run them up to a specific commit:
+```
+$ asv run 88fbbc33
+```
+
+To run them on a range of commits:
+```
+$ asv run 827f322b..729abcf3
+```
+
+To collate the benchmarks' results into a viewable website:
+```
+$ asv publish
+```
+This will create a tree of files in the `html` directory, but this cannot be viewed directly from the local filesystem, so we need to put them in a static site. `asv publish` also detects statistically significant decreases of performance, the results can be inspected in the 'Regression' tab of the static site.
+
+To visualise the results in a static site:
+```
+$ asv preview
+```
+To share the website on the internet, put the files in the `html` directory on any webserver that can serve static content (e.g. GitHub pages).
+
+To put the results in the `gh-pages` branch and push them to GitHub:
+```
+$ asv gh-pages
+```
+
+## Useful commands for managing the results
+
+To remove benchmarks from the database, for example, for a specific commit:
+
+```
+$ asv rm commit_hash=a802047be
+```
+
+This will remove the selected results from the files in the `results` directory. To update the results in the static site, remember to run `publish` again!
+
+See more options for `asv rm` in the [asv documentation](https://asv.readthedocs.io/en/stable/using.html#managing-the-results-database).
+
+To compare the results of running the benchmarks on two commits:
+```
+$ asv compare 88fbbc33 827f322b
+```
+
+### Writing benchmarks: `setup` and `setup_cache`
+
+- `setup` includes initialisation bits that should not be included
+in the timing of the benchmark. It can be added as:
+    - a method of a class, or
+    - an attribute of a free function, or
+    - a module-level setup function (run for every benchmark in the
+    module, prior to any function-specific setup)
+
+    If `setup` raises `NotImplementedError`, the benchmark is skipped
+
+- `setup_cache` only performs the setup calculation once
+(for each benchmark and each repeat) and caches the
+result to disk. This may be useful if the setup is computationally
+expensive.
+
+    A separate cache is used for each environment and each commit. The cache is thrown out between benchmark runs.
+
+    There are two options to persist the data for the benchmarks:
+    - `setup_cache` returns a data structure, which asv pickles to disk,
+        and then loads and passes as the first argument to each benchmark (not
+        automagically though), or
+    - `setup_cache` saves files to the cwd (which is a temp dir managed by
+        asv), which are then explicitly loaded in each benchmark. The recommended practice is to actually read the data in a `setup` function, so that loading time is not part of the benchmark timing.
+
+
+
+## Other handy commands
+To update the machine information
+```
+$ asv machine
+```
+
+To display results from the previous runs on the command line
+```
+$ asv show
+```
+
+To use binary search to find a commit within the benchmarked range that produced a large regression
+```
+$ asv find
+```
+Note this will only find the global maximum if runtimes over the range are more-or-less monotonic. See the [asv docs](https://asv.readthedocs.io/en/stable/using.html#finding-a-commit-that-produces-a-large-regression) for further details.
+
+To check the validity of written benchmarks
+```
+$ asv check
+```
+
+`asv` has features to run a given benchmark in the Python standard profiler `cProfile`, and then visualise the results in the tool of your choice. For example:
+```
+$ asv profile time_units.time_very_simple_unit_parse 10fc29cb
+```
+See the [asv docs on profiling](https://asv.readthedocs.io/en/stable/using.html#running-a-benchmark-in-the-profiler) for further details
+
+
+----
+## References
+- [astropy-benchmarks repository](https://github.com/astropy/astropy-benchmarks/tree/main)
+- [numpy benchmarks](https://github.com/numpy/numpy/tree/main/benchmarks/benchmarks)
+- [asv documentation](https://asv.readthedocs.io/en/stable/index.html)
---
+++ benchmarks/benchmarks/cellfinder.py
@@ -0,0 +1,273 @@
+import json
+import os
+import shutil
+from pathlib import Path
+
+from brainglobe_utils.IO.cells import save_cells
+
+from cellfinder.core.main import main as cellfinder_run
+from cellfinder.core.tools.IO import read_with_dask
+from cellfinder.core.tools.prep import prep_models
+from workflows.cellfinder import (
+    CellfinderConfig,
+    run_workflow_from_cellfinder_run,
+)
+from workflows.cellfinder import (
+    setup as setup_cellfinder_workflow,
+)
+from workflows.utils import DEFAULT_JSON_CONFIG_PATH_CELLFINDER
+
+
+class TimeBenchmark:
+    """
+
+    A base class for timing benchmarks for the cellfinder workflow.
+
+    It includes:
+     - a setup_cache function that downloads the GIN data specified in the
+       default_config.json to a local directory (created by asv). This function
+       runs only once before all repeats of the benchmark.
+    -  a setup function, that runs the setup steps for the workflow.
+    - a teardown function, that removes the output directory.
+
+    Notes
+    -----
+    The class includes some predefined attributes for timing benchmarks. For
+    the full list see
+    https://asv.readthedocs.io/en/stable/benchmarks.html#benchmark-attributes
+
+    Some asv benchmarking nomenclature:
+    - repeat: a benchmark repeat is made up of the following steps:
+      1- the `setup` is run,
+      2- then the timed benchmark routine is called for `n` iterations, and
+      3- finally that teardown function is run.
+      Each repeat generates a sample, which is the average time that the
+      routine took across all iterations. A new process is started for each
+      repeat of each benchmark. A calibration phase before running the repeat
+      computes the number of iterations that will be executed. Each benchmark
+      is run for a number of repeats. The setup_cache function is run only once
+      for all repeats of a benchmark (but it is discarded before the next
+      benchmark). By default `repeat` is set to 0, which means:
+        - if rounds==1 the default is
+            (min_repeat, max_repeat, max_time) = (1, 10, 20.0),
+        - if rounds != 1 the default is
+            (min_repeat, max_repeat, max_time) = (1, 5, 10.0)
+
+    - iterations (`number`): the number of iterations in each sample. Note that
+      `setup` and `teardown` are not run between iterations. asv will
+      automatically select the number of iterations so that each sample takes
+      approximately `sample_time` seconds.
+
+    - round: at each round, each benchmark is run for the specified number of
+      repeats. The idea is that we sample each benchmark over longer periods of
+      background performance variations.
+
+    - warmup time: asv will spend this time (in seconds) in calling the
+      benchmarked function repeatedly, before starting to run the actual
+      benchmark. If not specified, warmup_time defaults to 0.1 seconds
+
+    """
+
+    # Timing attributes
+    timeout = 600  # default: 60 s
+    version = (
+        None  # benchmark version. Default:None (i.e. hash of source code)
+    )
+    warmup_time = 0.1  # seconds
+    rounds = 2
+    repeat = 0
+    sample_time = 0.01  # default: 10 ms = 0.01 s;
+    min_run_count = 2  # default:2
+
+    # Input config file
+    # use environment variable CONFIG_PATH if exists, otherwise use default
+    input_config_path = os.getenv(
+        "CELLFINDER_CONFIG_PATH",
+        default=str(DEFAULT_JSON_CONFIG_PATH_CELLFINDER),
+    )
+
+    def setup_cache(self):
+        """
+        Download the input data from the GIN repository to the local
+        directory specified in the default_config.json.
+
+        Notes
+        -----
+        The `setup_cache` method only performs the computations once
+        per benchmark round and then caches the result to disk [1]_. It cannot
+        be parametrised [2]_. Therefore, if we sweep across different input
+        JSON files, we need to ensure all data for all configs is made
+        available with this setup function.
+
+
+        [1] https://asv.readthedocs.io/en/latest/writing_benchmarks.html#setup-and-teardown-functions
+        [2] https://asv.readthedocs.io/en/latest/writing_benchmarks.html#parameterized-benchmarks
+        """
+
+        # Check config file exists
+        assert Path(self.input_config_path).exists()
+
+        # Instantiate a CellfinderConfig from the input json file
+        # (fetches data from GIN if required)
+        with open(self.input_config_path) as cfg:
+            config_dict = json.load(cfg)
+        config = CellfinderConfig(**config_dict)
+
+        # Check paths to input data exist in config now
+        assert Path(config._signal_dir_path).exists()
+        assert Path(config._background_dir_path).exists()
+
+        # Ensure cellfinder model is downloaded to default path
+        _ = prep_models(
+            model_weights_path=config.model_weights,
+            install_path=None,  # Use default,
+            model_name=config.model,
+        )
+
+    def setup(self):
+        """
+        Run the cellfinder workflow setup steps.
+
+        The command line input arguments are injected as dependencies.
+        """
+
+        # Run setup
+        cfg = setup_cellfinder_workflow(self.input_config_path)
+
+        # Save configuration as attribute
+        self.cfg = cfg
+
+    def teardown(self):
+        """
+        Remove the cellfinder output directory.
+
+        The input data is kept for all repeats of the same benchmark,
+        to avoid repeated downloads from GIN.
+        """
+        shutil.rmtree(Path(self.cfg._output_path).resolve())
+
+
+class TimeFullWorkflow(TimeBenchmark):
+    """
+    Time the full cellfinder workflow.
+
+    It includes reading the signal and background arrays with dask,
+    detecting the cells and saving the results to an XML file
+
+    Parameters
+    ----------
+    TimeBenchmark : _type_
+        A base class for timing benchmarks for the cellfinder workflow.
+    """
+
+    def time_workflow(self):
+        run_workflow_from_cellfinder_run(self.cfg)
+
+
+class TimeReadInputDask(TimeBenchmark):
+    """
+    Time the reading input data operations with dask
+
+    Parameters
+    ----------
+    TimeBenchmark : _type_
+        A base class for timing benchmarks for the cellfinder workflow.
+    """
+
+    def time_read_signal_with_dask(self):
+        read_with_dask(str(self.cfg._signal_dir_path))
+
+    def time_read_background_with_dask(self):
+        read_with_dask(str(self.cfg._background_dir_path))
+
+
+class TimeDetectAndClassifyCells(TimeBenchmark):
+    """
+    Time the cell detection main pipeline (`cellfinder_run`)
+
+    Parameters
+    ----------
+    TimeBenchmark : _type_
+        A base class for timing benchmarks for the cellfinder workflow.
+    """
+
+    # extend basic setup function
+    def setup(self):
+        # basic setup
+        TimeBenchmark.setup(self)
+
+        # add input data as arrays to the config
+        self.signal_array = read_with_dask(str(self.cfg._signal_dir_path))
+        self.background_array = read_with_dask(
+            str(self.cfg._background_dir_path)
+        )
+
+    def time_cellfinder_run(self):
+        cellfinder_run(
+            self.signal_array,
+            self.background_array,
+            self.cfg.voxel_sizes,
+            self.cfg.start_plane,
+            self.cfg.end_plane,
+            self.cfg.trained_model,
+            self.cfg.model_weights,
+            self.cfg.model,
+            self.cfg.batch_size,
+            self.cfg.n_free_cpus,
+            self.cfg.network_voxel_sizes,
+            self.cfg.soma_diameter,
+            self.cfg.ball_xy_size,
+            self.cfg.ball_z_size,
+            self.cfg.ball_overlap_fraction,
+            self.cfg.log_sigma_size,
+            self.cfg.n_sds_above_mean_thresh,
+            self.cfg.soma_spread_factor,
+            self.cfg.max_cluster_size,
+            self.cfg.cube_width,
+            self.cfg.cube_height,
+            self.cfg.cube_depth,
+            self.cfg.network_depth,
+        )
+
+
+class TimeSaveCells(TimeBenchmark):
+    # extend basic setup function
+    def setup(self):
+        # basic setup
+        TimeBenchmark.setup(self)
+
+        # add input data as arrays to config
+        self.signal_array = read_with_dask(str(self.cfg._signal_dir_path))
+        self.background_array = read_with_dask(
+            str(self.cfg._background_dir_path)
+        )
+
+        # detect cells
+        self.detected_cells = cellfinder_run(
+            self.signal_array,
+            self.background_array,
+            self.cfg.voxel_sizes,
+            self.cfg.start_plane,
+            self.cfg.end_plane,
+            self.cfg.trained_model,
+            self.cfg.model_weights,
+            self.cfg.model,
+            self.cfg.batch_size,
+            self.cfg.n_free_cpus,
+            self.cfg.network_voxel_sizes,
+            self.cfg.soma_diameter,
+            self.cfg.ball_xy_size,
+            self.cfg.ball_z_size,
+            self.cfg.ball_overlap_fraction,
+            self.cfg.log_sigma_size,
+            self.cfg.n_sds_above_mean_thresh,
+            self.cfg.soma_spread_factor,
+            self.cfg.max_cluster_size,
+            self.cfg.cube_width,
+            self.cfg.cube_height,
+            self.cfg.cube_depth,
+            self.cfg.network_depth,
+        )
+
+    def time_save_cells(self):
+        save_cells(self.detected_cells, self.cfg._detected_cells_path)
