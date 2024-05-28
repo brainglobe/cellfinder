7ad06c6ba0927780251cd21f50bfec5da05fd827
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2024-05-28 13:59:43 +0100

    Always set KERAS_BACKEND to torch on init

--- cellfinder/__init__.py
+++ cellfinder/__init__.py
@@ -25,9 +25,7 @@ except PackageNotFoundError as e:
 
 
 # If no backend is configured and installed for Keras, tools cannot be used
-# Check if backend is configured. If not, set to "torch"
-if not os.getenv("KERAS_BACKEND"):
-    os.environ["KERAS_BACKEND"] = "torch"
+os.environ["KERAS_BACKEND"] = "torch"
 
 # Check backend is installed
 backend = os.getenv("KERAS_BACKEND")
