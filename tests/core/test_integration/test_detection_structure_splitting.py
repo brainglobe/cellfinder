352aa404a3a488c672465d2e20e489ac71ffad71
Matt Einhorn <matt@einhorn.dev>
2024-06-10 04:55:05 -0400

    Prevent zero underflow in volume (#437)
    * Prevent zero underflow in volume.
    
    * Update tests/core/test_integration/test_detection_structure_splitting.py
    
    Co-authored-by: Alessandro Felder <alessandrofelder@users.noreply.github.com>
    
    * Now that point is fully inside, it'll all shifted upward by one.
    
    ---------
    
    Co-authored-by: Alessandro Felder <alessandrofelder@users.noreply.github.com>

--- tests/core/test_integration/test_detection_structure_splitting.py
+++ tests/core/test_integration/test_detection_structure_splitting.py
@@ -8,9 +8,13 @@ real life data.
 
 import os
 
+import numpy as np
 import pytest
 from brainglobe_utils.IO.image.load import read_with_dask
 
+from cellfinder.core.detect.filters.volume.structure_splitting import (
+    split_cells,
+)
 from cellfinder.core.main import main
 
 data_dir = os.path.join(
@@ -46,3 +50,34 @@ def test_structure_splitting(signal_array, background_array):
         voxel_sizes,
         n_free_cpus=0,
     )
+
+
+def test_underflow_issue_435():
+    # two cells centered at (9, 10, 10), (19, 10, 10) with radius 5
+    p1 = np.array([9, 10, 10])
+    p2 = np.array([19, 10, 10])
+    radius = 5
+
+    bright_voxels = np.zeros((30, 20, 20), dtype=np.bool_)
+
+    pos = np.empty((30, 20, 20, 3))
+    pos[:, :, :, 0] = np.arange(30).reshape((-1, 1, 1))
+    pos[:, :, :, 1] = np.arange(20).reshape((1, -1, 1))
+    pos[:, :, :, 2] = np.arange(20).reshape((1, 1, -1))
+
+    dist1 = pos - p1.reshape((1, 1, 1, 3))
+    dist1 = np.sqrt(np.sum(np.square(dist1), axis=3))
+    inside1 = dist1 <= radius
+    dist2 = pos - p2.reshape((1, 1, 1, 3))
+    dist2 = np.sqrt(np.sum(np.square(dist2), axis=3))
+    inside2 = dist2 <= radius
+
+    bright_voxels[np.logical_or(inside1, inside2)] = True
+    bright_indices = np.argwhere(bright_voxels)
+
+    centers = split_cells(bright_indices)
+
+    # for some reason, same with pytorch, it's shifted by 1. Probably rounding
+    expected = {(10, 11, 11), (20, 11, 11)}
+    got = set(map(tuple, centers.tolist()))
+    assert expected == got
