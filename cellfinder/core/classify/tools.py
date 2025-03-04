b9627450a0c4fd297db4f70a086f25b4e2e46759
nischitkumar <nischitkumar@gmail.com>
2025-03-04 17:32:35 +0530

    Modified train_yml to train_yaml in the train.py file on both lines 10 and 29

--- cellfinder/core/classify/tools.py
+++ cellfinder/core/classify/tools.py
@@ -48,10 +48,7 @@ def get_model(
             )
             if model_weights is None:
                 raise OSError("`model_weights` must be provided")
-            try:
-                model.load_weights(model_weights)
-            except OSError:
-                print("The provided weights do not match the expected model architecture. Ensure that you are using the correct model definition when loading weights.")
+            model.load_weights(model_weights)
         return model
 
 
