d58d642c167263f4587c49c351cb344d25ba527c
Prisha Sharma <140067623+parharti@users.noreply.github.com>
2025-04-11 20:04:19 +0530

    Update test_utils.py

--- tests/napari/test_utils.py
+++ tests/napari/test_utils.py
@@ -72,3 +72,15 @@ def test_html_label_widget():
     label_widget = html_label_widget("A nice label", tag="h1")
     assert label_widget["widget_type"] == "Label"
     assert label_widget["label"] == "<h1>A nice label</h1>"
+
+def test_get_plane_size_in_memory_numpy():
+    arr = np.zeros((10, 256, 256), dtype=np.uint16) 
+    expected_size=((256*256*2)*1.1)/(1024**2)
+    result = get_plane_size_in_memory(arr)
+    assert abs(result - expected_size) < 0.01
+
+def test_get_plane_size_in_memory_dask():
+    darr = da.zeros((5, 128, 128), chunks=(1, 128, 128), dtype=np.uint8)
+    expected_size = ((128 * 128 * 1) * 1.1) / (1024 ** 2)
+    result = get_plane_size_in_memory(darr)
+    assert abs(result - expected_size) < 0.01
