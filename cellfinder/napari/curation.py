3b5100dfc5df3155e68c3a219d5af15858f5b65f
alessandrofelder <alessandrofelder@users.noreply.github.com>
2025-04-30 16:54:17 +0100

    make check_training_data_exists more strict, and test it

--- cellfinder/napari/curation.py
+++ cellfinder/napari/curation.py
@@ -496,23 +496,24 @@ class CurationWidget(QWidget):
 
     def check_training_data_exists(self) -> bool:
         if not (
-            self.training_data_cell_layer or self.training_data_non_cell_layer
+            self.training_data_cell_layer and self.training_data_non_cell_layer
         ):
             show_info(
                 "No training data layers have been added. "
-                "Please add a layer and annotate some points.",
+                "Please add layers for both cells and non-cells,"
+                "and annotate some points in both.",
             )
             return False
         else:
             if (
                 len(self.training_data_cell_layer.data) > 0
-                or len(self.training_data_non_cell_layer.data) > 0
+                and len(self.training_data_non_cell_layer.data) > 0
             ):
                 return True
             else:
                 show_info(
                     "No training data points have been added. "
-                    "Please annotate some points.",
+                    "Please annotate points in both training data layers.",
                 )
                 return False
 
