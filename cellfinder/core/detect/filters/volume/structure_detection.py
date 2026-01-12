Fix cell label overflow using dtype max

cellfinder/core/detect/filters/volume/structure_detection.py
@@ -103,6 +103,7 @@ spec = [
     ("z", vol_numba_type),
     ("next_structure_id", sid_numba_type),
     ("soma_centre_value", sid_numba_type),  # as large as possible
+    ("max_label", sid_numba_type),
     ("shape", types.UniTuple(vol_numba_type, 2)),
     ("obsolete_ids", DictType(sid_numba_type, sid_numba_type)),
     ("coords_maps", DictType(sid_numba_type, list_of_points_type)),
@@ -145,6 +146,7 @@ class CellDetector:
         width: int,
         start_z: int,
         soma_centre_value: sid_numba_type,
+        max_label: sid_numba_type,
     ):
         """
         Parameters
@@ -158,6 +160,7 @@ class CellDetector:
         self.z = start_z
         self.next_structure_id = 1
         self.soma_centre_value = soma_centre_value
+        self.max_label = max_label
 
         # Mapping from obsolete IDs to the IDs that they have been
         # made obsolete by
@@ -208,6 +211,10 @@ class CellDetector:
             with their structure ID. Plane is in Y, X axis order.
         """
         soma_centre_value = self.soma_centre_value
+
+        with objmode(max_label="u8"):
+            max_label = np.iinfo(plane.dtype).max
+
         for y in range(plane.shape[0]):
             for x in range(plane.shape[1]):
                 if plane[y, x] == soma_centre_value:
@@ -222,10 +229,9 @@ class CellDetector:
                         neighbour_ids[2] = previous_plane[y, x]
 
                     if is_new_structure(neighbour_ids):
-                        if (self.soma_centre_value == 65535 and
-                            self.next_structure_id > 65535):
+                        if self.next_structure_id > max_label:
                             raise ValueError(
-                                "uint16 label overflow: number of connected "
+                                "label overflow: number of connected "
                                 "components exceeds label capacity"
                             )
                         neighbour_ids[0] = self.next_structure_id

cellfinder/core/detect/filters/volume/volume_filter.py
@@ -99,12 +99,13 @@ class VolumeFilter:
         )
 
         self.z = settings.start_plane + self.ball_filter.first_valid_plane
-
+        max_label = np.iinfo(settings.detection_dtype).max
         self.cell_detector = CellDetector(
             settings.plane_height,
             settings.plane_width,
             start_z=self.z,
             soma_centre_value=settings.detection_soma_centre_value,
+            max_label=max_label,
         )
         # make sure we load enough data to filter. Otherwise, we won't be ready
         # to filter and the data loading thread will wait for data to be

tests/core/test_unit/test_detect/test_filters/test_volume_filters/test_connected_components_labelling.py
@@ -51,7 +51,10 @@ def test_connect_four_limits(
         actual_nonzeros == expected_nonzeros
     ), "Checkerboard didn't have the expected number of non-zeros"
 
-    cell_detector = CellDetector(height, width, 0, soma_centre_value)
+    max_label = np.iinfo(datatype).max
+    cell_detector = CellDetector(
+        height, width, 0, soma_centre_value, max_label
+    )
     labelled_plane = cell_detector.connect_four(checkerboard, None)
     one_count = np.count_nonzero(labelled_plane == 1)
 
@@ -77,7 +80,10 @@ def test_connect_four_uint16_overflow(linear_size: int) -> None:
     j = np.arange(width)[np.newaxis, :]
     checkerboard[(i + j) % 2 == 0] = soma_centre_value
 
-    cell_detector = CellDetector(height, width, 0, soma_centre_value)
+    max_label = np.iinfo(datatype).max
+    cell_detector = CellDetector(
+        height, width, 0, soma_centre_value, max_label
+    )
 
-    with pytest.raises(ValueError, match="uint16|overflow|label"):
+    with pytest.raises(ValueError, match="overflow|label"):
         cell_detector.connect_four(checkerboard, None)